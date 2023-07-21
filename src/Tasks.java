e59c34c89025e261643f343d877fd11ef90cadc0
believer4U- <saady6nov@gmail.com>
2023-07-21 15:03:11 +0100

    completed Task 3

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Tasks {
     public static void main(String[] a){
+            // TASK 1
 //        String firstName;
 //        String lastName;
 //        firstName = "Saad";
@@ -7,15 +10,31 @@ public class Tasks {
 //        String name = firstName + lastName;
 //        System.out.println(name);
 
-        int num1, num2;
-        num1 = 47;num2 = 3_000;
-        System.out.println(num1 + num2);
+            // TASK 2
+//        int num1, num2;
+//        num1 = 47;num2 = 3_000;
+//        System.out.println(num1 + num2);
+//
+//        double result = (double)num2/(double)47;
+//        System.out.println(result);
+//
+//        int num3 = 8;
+//        System.out.println(num3++);//prints first, then increment
+//        System.out.println(++num3);//first increments, then prints
 
-        double result = (double)num2/(double)47;
-        System.out.println(result);
+            // TASK 3
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your first name: ");
+        String firstName = sc.next();
+        System.out.println("Please enter your last name: ");
+        String lastName = sc.next();
+        System.out.println("Welcome " + firstName + " " + lastName + " to my java Demo");
 
-        int num3 = 8;
-        System.out.println(num3++);//prints first, then increment
-        System.out.println(++num3);//first increments, then prints
+        System.out.println("Please enter a number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Please enter another number: ");
+        int num2 = sc.nextInt();
+        int result = num1 + num2;
+        System.out.println("The Sum is .. (drumroll) ... : " + result);
     }
 }
