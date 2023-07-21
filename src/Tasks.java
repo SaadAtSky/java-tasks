ca09a20a3d9c38f6749ed0144617ca04f1948ff7
believer4U- <saady6nov@gmail.com>
2023-07-21 14:45:08 +0100

    completed Tasks 1 and Tasks 2

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,10 +1,21 @@
 public class Tasks {
     public static void main(String[] a){
-        String firstName;
-        String lastName;
-        firstName = "Saad";
-        lastName = "Ahmad";
-        String name = firstName + lastName;
-        System.out.println(name);
+//        String firstName;
+//        String lastName;
+//        firstName = "Saad";
+//        lastName = "Ahmad";
+//        String name = firstName + lastName;
+//        System.out.println(name);
+
+        int num1, num2;
+        num1 = 47;num2 = 3_000;
+        System.out.println(num1 + num2);
+
+        double result = (double)num2/(double)47;
+        System.out.println(result);
+
+        int num3 = 8;
+        System.out.println(num3++);//prints first, then increment
+        System.out.println(++num3);//first increments, then prints
     }
 }
